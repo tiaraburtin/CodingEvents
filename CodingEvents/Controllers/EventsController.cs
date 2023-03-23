25d00db6f2ad9f3096ed5f0c893b8b0ae20139e9
Tiara <tburtin1019@gmail.com>
2023-03-23 11:19:55 -0500

    Made a template page!

---
+++ CodingEvents/Controllers/EventsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CodingEvents.Controllers
+{
+    public class EventsController : Controller
+    {
+        private static List<string> Events = new List<string>();
+
+        public IActionResult Index()
+        { string AirTest = "Test Stuff";
+
+            
+            //Events.Add("Code With Pride");
+            //Events.Add("Strange Loops");
+            //Events.Add("Women Who Code");
+
+            ViewBag.events = Events;
+
+            ViewBag.balloon = AirTest;
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        [Route("/Events/Add")]
+
+        public IActionResult NewEvent(string name)
+        {
+            Events.Add(name);
+            return Redirect("/Events");
+                }
+
+
+    }
+}
